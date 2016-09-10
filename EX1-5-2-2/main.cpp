db5b473706ccc157a46c74b17e4a78e02ae31342
Yu-Hsuan Guan <yhguan8128@gmail.com>
2016-09-10 20:37:50 +0800

    + file and 1-5-2-2

--- EX1-5-2-2/main.cpp
+++ EX1-5-2-2/main.cpp
@@ -5,23 +5,23 @@
 
 using namespace std;
 
-void showArr(int * a, int n)
+void showVec(vector<int> a)
 {
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < a.size(); ++i)
     {
         printf("[%d]", a[i]);
     }
     printf("\n");
 }
 
-void showAllTrueFalse(int n, int * a)
+void showAllTrueFalse(int n, vector<int> a)
 {
     if (n == 1)
     {
         a[0] = 0;
-        showArr(a, 4);
+        showVec(a);
         a[0] = 1;
-        showArr(a, 4);
+        showVec(a);
         return;
     }
     a[n-1] = 0;
@@ -30,10 +30,14 @@ void showAllTrueFalse(int n, int * a)
     showAllTrueFalse(n-1, a);
 }
 
+void showAllTrueFalse(int n)
+{
+    showAllTrueFalse(n, vector<int>(n,0));
+}
+
 int main(int argc, const char * argv[])
 {
-    int a[4] = {0};
-    showAllTrueFalse(4, a);
+    showAllTrueFalse(4);
 
     return 0;
 }
