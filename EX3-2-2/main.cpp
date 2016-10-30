b8dcad19c8629e264911d11fa14e3fee44adfb87
Yu-Hsuan Guan <yhguan8128@gmail.com>
2016-10-30 20:16:38 +0800

    modified

--- EX3-2-2/main.cpp
+++ EX3-2-2/main.cpp
@@ -5,13 +5,14 @@
 #include <memory.h>
 using namespace std;
 
+#define CARS_NUM 3
+int Moves[CARS_NUM*2+1] = {0};
 stack<int> Cars;
-int Moves[7] = {0};
 
 void MoveCars()
 {
     int c = 0;
-    for (int i = 1; i <= 6; ++i)
+    for (int i = 1; i <= CARS_NUM*2; ++i)
     {
         if (Moves[i] == 1)
         {
@@ -26,41 +27,48 @@ void MoveCars()
     }
     printf("\n");
 }
-// h: # of pushs, p: # of pops
-// c: total # of pushs or cursor of cars, n: total # of steps
-void ArrangeCars(int h, int p, int c, int n)
+// h: # of pushs
+// p: # of pops
+// c: total # of pushs or cursor of cars
+// m: total # of steps
+void ArrangeCars(int h, int p, int c, int m)
 {
-    if (c == 3 & h > 0)
+    if (c == CARS_NUM & h > 0)
     {
-        Moves[n+1] = 2;
+        Moves[m+1] = 2;
         if (h == 1)
         {
-            Moves[6] = 2;
+            Moves[CARS_NUM*2] = 2;
             MoveCars();
             return;
         }
-        ArrangeCars(h-1, p+1, 3, n+1);
+        ArrangeCars(h-1, p+1, CARS_NUM, m+1);
     }
 
-    if (c < 3 & h == 0)
+    if (c < CARS_NUM & h == 0)
     {
-        Moves[n+1] = 1;
-        ArrangeCars(1, p, c+1, n+1);
+        Moves[m+1] = 1;
+        ArrangeCars(1, p, c+1, m+1);
     }
 
-    if (c < 3 & h > 0)
+    if (c < CARS_NUM & h > 0)
     {
-        Moves[n+1] = 1;
-        ArrangeCars(h+1, p, c+1, n+1);
-        Moves[n+1] = 2;
-        ArrangeCars(h-1, p+1, c, n+1);
+        Moves[m+1] = 1;
+        ArrangeCars(h+1, p, c+1, m+1);
+        Moves[m+1] = 2;
+        ArrangeCars(h-1, p+1, c, m+1);
     }
+}
+
+void Arrange(int n)
+{
+    Moves[1] = 1;
+    ArrangeCars(1, 0, 1, 1);
 }
 
 int main(int argc, const char * argv[])
 {
-    Moves[1] = 1;
-    ArrangeCars(1, 0, 1, 1);
+    Arrange(CARS_NUM);
 
     return 0;
 }
